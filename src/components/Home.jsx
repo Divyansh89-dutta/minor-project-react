326379365186d2b48467f65f3678c7ad6b6d2b54
Divyansh89-dutta <divyanshduttaroy163@gmail.com>
2025-01-14 12:54:47 +0530

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Sidenav from './templates/Sidenav'
+
+function Home() {
+    document.title = "Movie | Homepage"
+  return (
+    <>
+      <Sidenav />
+    <div className='w-[80%] h-full'></div>
+    </>
+  )
+}
+
+export default Home
---
+++ src/components/templates/Sidenav.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Sidenav() {
+  return (
+    <>
+    <div className='w-[20%] h-full border-r-2 border-zinc-400 p-3'>
+        <h1 className='text-xl text-zinc-400 flex items-center font-bold'><i class="text-[#6556CD] text-2xl ri-tv-fill"></i>Divyansh</h1>
+    </div>
+    </>
+  )
+}
+
+export default Sidenav
